d292bf13b833e6f847012a3877167a11ee181920
patientdevtech <patientdevtech@gmail.com>
2022-09-12 17:42:44 -0400

    image links updated in Stories.js

--- components/Stories.js
+++ components/Stories.js
@@ -1,75 +1,56 @@
 import StoryCard from "./StoryCard"
 const stories = [
-    // {
-    //     name: "Patient Devtech",
-    //     src: "👸",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Planner Friend",  
-    //     src: "🤓",
-    //     profile: "",
-    // },    
-    // {
-    //     name: "Famous Friend",
-    //     src: "🤩",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Empath Friend",
-    //     src: "🥲",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Workaholic Friend",
-    //     src: "🤖",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Party Friend",
-    //     src: "🥳",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Goody-Goody Friend",
-    //     src: "",
-    //     profile: "/🧐",
-    // },
-    // {
-    //     name: "OverShare Friend",
-    //     src: "😩",
-    //     profile: "",
-    // },
-    // {
-    //     name: "Thaaaat Friend",
-    //     src: "🥴",
-    //     profile: "",
-    // },
     {
+        name: "Patient Devtech",
+        src: "",
+        profile: "https://imgur.com/qmDwxqs.png",
+    },
+    {
+        name: "Planner Friend",  
+        src: "",
+        profile: "https://imgur.com/r0n6msN.png",
+    },    
+    {
+        name: "Famous Friend",
+        src: "",
+        profile: "https://imgur.com/mGX739w.png",
+    },
+    {
+        name: "Car Pics Friend",
+        src: "",
+        profile: "https://imgur.com/SEcPvDJ.png",
+    },
+    {
+        name: "Plant Friend",
+        src: "",
+        profile: "https://imgur.com/MvwillM.png",
+    },
+    {
+        name: "Party Friend",
+        src: "",
+        profile: "https://imgur.com/bGMczBx.png",
+    },
+    {
+        name: "Goody-Goody Friend",
+        src: "",
+        profile: "https://imgur.com/pY9B0Cx.png",
+    },
+    {
+        name: "OverShare Friend",
+        src: "",
+        profile: "https://imgur.com/QcyTZFP.png",
+    },
+    {
+        name: "Thaaaat Friend",
+        src: "",
+        profile: "https://imgur.com/xR9idiv.png",
+    },
+      {
         name: "Billy G",
         src: "https://links.papareact.com/4u4",
         profile: "https://links.papareact.com/zvy",
     },
-    {
-        name: "Billy G",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://links.papareact.com/zvy",
-    }, 
-    {
-        name: "Billy G",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://links.papareact.com/zvy",
-    },
-    {
-        name: "Billy G",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://links.papareact.com/zvy",
-    },
-    {
-        name: "B G",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://imgur.com/P7lFBCc",
-    },
+  
 ]
 
 function Stories() {
