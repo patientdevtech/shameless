ee8bae9d0e61ce6bb6fbb05d21bd59910a104e3a
patientdevtech <patientdevtech@gmail.com>
2022-09-12 20:02:07 -0400

    nextauth url updated

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-// import { getSession } from "next-auth/client" 
+import { unstable_getServerSession } from "next-auth/next" 
 import Head from 'next/head'
 import Header from "../components/Header"
 import Login from "../components/Login";
@@ -7,9 +7,10 @@ import Feed from "../components/Feed"
 import Connections from "../components/Connections"
 
 
+
 export default function Home({ session }) {
   //if there is no session, return a login component 
-  // if (!session) return <Login />;
+  if (!session) return <Login />;
   return (
     <div className="h-screen bg-tan-100 overflow-hidden">
       <Head>
@@ -40,13 +41,13 @@ export default function Home({ session }) {
 // }
 
 //uncomment out vvvvvvvvvvv before 1:18 https://www.youtube.com/watch?v=dBotWYKYYWc
-// export async function getServerSideProps (context) {
-//  const session = await unstable_getServerSession(context);
+export async function getServerSideProps (context) {
+ const session = await unstable_getServerSession(context);
  
-//  return{
-//   props: {
-//     session
-//   }
-//  }
-// }
+ return{
+  props: {
+    session
+  }
+ }
+}
 //uncomment out ^^^^^^^^^^^^^^^^
