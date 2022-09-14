6e6ee43291c3f8b2b27bb704e50c2f5ffe55d661
patientdevtech <patientdevtech@gmail.com>
2022-09-13 20:19:45 -0400

    index.js modified to basic features to retry oauth setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import { unstable_getServerSession } from "next-auth/next" 
-// import NextAuth from 'pages/api/auth/[...nextauth]'
-import { getSession } from 'next-auth/react';
+// import { getSession } from "next-auth/client" 
 import Head from 'next/head'
 import Header from "../components/Header"
 import Login from "../components/Login";
@@ -9,11 +7,9 @@ import Feed from "../components/Feed"
 import Connections from "../components/Connections"
 
 
-
-
 export default function Home({ session }) {
   //if there is no session, return a login component 
-  if (!session) return <Login />;
+  // if (!session) return <Login />;
   return (
     <div className="h-screen bg-tan-100 overflow-hidden">
       <Head>
@@ -31,19 +27,6 @@ export default function Home({ session }) {
   );
 }
 
-export async function getServerSideProps (context) {
-  const session = await unstable_getServerSession(context);
-  const session = await unstable_getServerSession(context.req, context.res);
- 
-  return{
-   props: {
-     session
-   }
-  }
- }
-
-
-//1 uncomment out vvvvvvvvvvv before 1:18 https://www.youtube.com/watch?v=dBotWYKYYWc
 // export async function getServerSideProps(context) {
 //   //get the user from nextauth library. context is user request.
 //   //session info is passed as a prop in user's browser  
@@ -55,39 +38,11 @@ export async function getServerSideProps (context) {
 //     }
 //   }
 // }
-//uncomment out ^^^^^^^^^^^^^^^^
 
-//2 uncomment out vvvvvvvvvvv https://next-auth.js.org/v3/getting-started/client
-// import { getProviders } from "next-auth/client"
-
-// export default async (req, res) => {
-//   const providers = await getProviders()
-//   console.log("Providers", providers)
-//   res.end()
-// }
-//uncomment out ^^^^^^^^^^^^^^^^
-
-//3 uncomment out vvvvvvvvvvv before 1:18 https://www.youtube.com/watch?v=dBotWYKYYWc
-//context contains request information & response.  It is the request the user sends through when they go to the connected forever ever site.
-// return passes user information to component as a prop which renders all info onto user's browser  
-// export async function getServerSideProps(context) {
-//   //get the user
-//   const session = await getSession(context);
-//   //return session properties from server
-//   return {
-//     props: {
-//       session
-//     }
-//   }
-// }
-//uncomment out ^^^^^^^^^^^^^^^^
-
-
-// 4 uncomment out vvvvvvvvvvv tutoring session 9.12
+//uncomment out vvvvvvvvvvv before 1:18 https://www.youtube.com/watch?v=dBotWYKYYWc
 // export async function getServerSideProps (context) {
-//  let session = await unstable_getServerSession(context.req, context.res);
-//  session = JSON.stringify(session);
-
+//  const session = await unstable_getServerSession(context);
+ 
 //  return{
 //   props: {
 //     session
